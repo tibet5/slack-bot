6d9a4a73e76448ee384d610e62eaf7f744580283
Tibet Celik <tibetcelik2@gmail.com>
2021-10-23 15:10:59 +0300

    .env file added

--- slack_bot/slack_bot/settings.py
+++ slack_bot/slack_bot/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0%k4n4gvb-#eb&2(i*k0m88ry(7e!juw@c_%3v*ln!1fc$q(2z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
